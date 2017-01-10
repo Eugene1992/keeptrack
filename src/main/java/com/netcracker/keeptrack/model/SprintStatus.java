26ad189a8344f10c933a98ae89cb9f39e102b6c5
Eugene1992 <deyneko55@gmail.com>
2017-01-10 21:19:51 +0200

    #9 Created Sprint and SprintStatus entities classes.

---
+++ src/main/java/com/netcracker/keeptrack/model/SprintStatus.java
@@ -0,0 +1,38 @@
+package com.netcracker.keeptrack.model;
+
+/**
+ * The statuses of the stages of sprint execution.
+ * Next sprint can be opened only after the closing of the previous sprint.
+ *
+ * @see Sprint
+ */
+public enum SprintStatus {
+
+    /**
+     * The sprint is created but not started because there is not a closed previous sprint.
+     */
+    CREATED("Created"),
+
+    /**
+     * Sprint is in the execution stage.
+     */
+    IN_PROGRESS("In progress"),
+
+    /**
+     * The sprint is closed.
+     */
+    CLOSED("Closed");
+
+    /**
+     * Sprint status title.
+     */
+    private String name;
+
+    SprintStatus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
