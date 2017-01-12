a5e21595db6d0ec099d4e690b60c9fe2f9254b96
Eugene1992 <deyneko55@gmail.com>
2017-01-12 22:22:05 +0200

    #25 Added Spring MVC support.

---
+++ src/main/java/com/netcracker/keeptrack/web/HomeController.java
@@ -0,0 +1,17 @@
+package com.netcracker.keeptrack.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Return Apache Tiles 'home' definition by '/' GET request.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "home";
+    }
+}
