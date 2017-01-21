ab8ca9d64d081aede270edd1f3f048bba959b43f
Eugene1992 <deyneko55@gmail.com>
2017-01-21 16:42:59 +0200

    #47 Added Spring security framework.

---
+++ src/main/java/com/netcracker/keeptrack/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,45 @@
+package com.netcracker.keeptrack.service.impl;
+
+import com.netcracker.keeptrack.model.User;
+import com.netcracker.keeptrack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * UserDetailsService implementation which allows to perform user
+ * authentication on the basis of data obtained from UserService.
+ *
+ */
+@Service("user-details")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * On the basis of the user credentials create {@link UserDetails} object which
+     * will check the entered user login and password, and then authenticate the user or not.
+     * @param username - username credential.
+     * @return {@link UserDetails} instance
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+        Set<GrantedAuthority> roles = new HashSet();
+        roles.add(new SimpleGrantedAuthority(user.getRole().name()));
+        UserDetails userDetails =
+                new org.springframework.security.core.userdetails.User(
+                        user.getUsername(),
+                        user.getPassword(),
+                        roles);
+        return userDetails;
+    }
+}
--- src/main/java/com/netcracker/keeptrack/web/HomeController.java
+++ src/main/java/com/netcracker/keeptrack/web/HomeController.java
@@ -1,8 +1,14 @@
 package com.netcracker.keeptrack.web;
 
+import com.netcracker.keeptrack.model.User;
+import com.netcracker.keeptrack.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
 
 /**
  * Return Apache Tiles 'home' definition by '/' GET request.
@@ -10,12 +16,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Login page controller.
+     * @return tiles 'home' definition
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
     /**
      * Home page controller.
      * @return tiles 'home' definition
      */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Principal principal, Model model) {
+        User loggedUser = userService.getUserByUsername(principal.getName());
+        model.addAttribute("loggedUser", loggedUser);
         return "home";
     }
 }
